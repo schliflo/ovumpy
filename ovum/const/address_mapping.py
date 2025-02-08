fe49990c1331aa2d444516a208e1bb1ec12023a4
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2025-02-08 17:51:38 +0100

    manually add more params

--- ovum/const/address_mapping.py
+++ ovum/const/address_mapping.py
@@ -1,6 +1,41 @@
 address_map = {
+    450: {
+        'label': 'Autarkiegrad im Überschussbetrieb ',
+        'factor': 1,
+        'unit': '%',
+    },
+    456: {
+        'label': 'PV-Watch Messwert',
+        'factor': .100,
+        'unit': 'kW',
+    },
+    500: {
+        'label': 'Betriebsart Heizung',
+        'factor': 1,
+        'unit': '',
+    },
+    503: {
+        'label': 'Vorlauftemperatur Heizkreis 1',
+        'factor': .1,
+        'unit': '°C',
+    },
+    504: {
+        'label': 'Rücklauftemperatur Heizkreis 1',
+        'factor': .1,
+        'unit': '°C',
+    },
     505: {
         'label': 'Temperatur Heizungsspeicher',
+        'factor': .1,
+        'unit': '°C',
+    },
+    506: {
+        'label': 'Temperatur Heizungsspeicher',
+        'factor': .1,
+        'unit': '°C',
+    },
+    524: {
+        'label': 'Raumsolltemperatur Heizkreis 1',
         'factor': .1,
         'unit': '°C',
     },
--- ovum/test.py
+++ ovum/test.py
@@ -8,6 +8,13 @@ async def main():
     client = OvumClient()
     # value = await client.get_address(505)
     logging.info(f'{await client.get_address(5)}')
+    logging.info(f'{await client.get_address(450)}')
+    logging.info(f'{await client.get_address(456)}')
+    logging.info(f'{await client.get_address(500)}')
+    logging.info(f'{await client.get_address(503)}')
+    logging.info(f'{await client.get_address(504)}')
     logging.info(f'{await client.get_address(505)}')
+    logging.info(f'{await client.get_address(506)}')
+    logging.info(f'{await client.get_address(524)}')
 
 asyncio.run(main())
